e9fa4d8b2a392da2b625e9f5ae3e719122001b74
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-19 16:41:05 +0530

    Added gofile upload

--- main.py
+++ main.py
@@ -5,8 +5,12 @@
 # License -> https://github.com/FayasNoushad/GoFile-Bot/blob/main/LICENSE
 
 import os
-from pyrogram import Client
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+from gofile import uploadFile
 
+
+DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
 
 Bot = Client(
     "GoFile-Bot",
@@ -14,6 +18,51 @@ Bot = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
+
+
+@Bot.on_message(filters.private & filters.media)
+async def media_filter(bot, update):
+    medianame = DOWNLOAD_LOCATION + str(update.from_user.id)
+    try:
+        message = await update.reply_message(
+            text="`Processing...`",
+            quote=True,
+            disable_web_page_preview=True
+        )
+        await update.download()
+        response = uploadFile(medianame)
+        try:
+            os.remove(medianame)
+        except:
+            pass
+    except Exception as error:
+        await update.reply_text(
+            text=f"Error :- <code>{error}</code>",
+            quote=True,
+            disable_web_page_preview=True
+        )
+        return
+    text = f"**File Name:** `{response["fileName"]}`" + "\n"
+    text += f"**Download Page:** `{response["downloadPage"]}`" + "\n"
+    text += f"**Direct Download Link:** `{response["directLink"]}`" + "\n"
+    text += f"**Info:** `{response["info"]}`"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="Open Link", url=response["directLink"]),
+                InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url={response["directLink"]}")
+            ],
+            [
+                InlineKeyboardButton(text="Join Updates Channel", url="https://telegram.me/FayasNoushad")
+            ]
+        ]
+    )
+    await message.edit_text(
+        text=text,
+        reply_markup=reply_markup,
+        quote=True,
+        disable_web_page_preview=True
+    )
 
 
 Bot.run()
