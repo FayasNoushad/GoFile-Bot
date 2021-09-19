99832c1488dbd5a1d3d9b71b9c8632216c012521
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-19 16:21:16 +0530

    Added Bot for run

--- main.py
+++ main.py
@@ -3,3 +3,17 @@
 # Copyright permission under MIT License
 # All rights reserved by FayasNoushad
 # License -> https://github.com/FayasNoushad/GoFile-Bot/blob/main/LICENSE
+
+import os
+from pyrogram import Client
+
+
+Bot = Client(
+    "GoFile-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+Bot.run()
