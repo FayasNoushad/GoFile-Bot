1213315970278e57f8852652d0d52179e4c27597
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-19 16:19:50 +0530

    Added main file

---
+++ main.py
@@ -0,0 +1,5 @@
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/GoFile-Bot/blob/main/LICENSE
