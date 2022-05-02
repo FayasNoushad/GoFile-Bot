9a343645b159eaa1f46b9dc89a7a3b04a6d4aacd
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2022-05-02 15:30:29 +0530

    Update gofile errors

--- gofile.py
+++ gofile.py
@@ -1,21 +1,22 @@
 import requests
 
 def uploadFile(file: str):
+    
     server = requests.get("https://api.gofile.io/getServer").json()["data"]["server"]
+    
     response = requests.post(
         url=f"https://{server}.gofile.io/uploadFile",
         data={
             "token": None,
-            "folderId": None,
-            "description": None,
-            "password": None,
-            "tags": None,
-            "expire": None
+            "folderId": None
         },
         files={"upload_file": open(file, "rb")}
     ).json()
+    
     if response["status"] == "ok":
-        return response["data"]
+        data = response["data"]
+        data["directLink"] = f"https://{server}.gofile.io/download/{data['fileId']}"
+        return data
     elif "error-" in response["status"]:
         error = response["status"].split("-")[1]
         raise Exception(error)
--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import os
 import urldl
+from gofile import uploadFile
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
-from gofile import uploadFile
 
 
 Bot = Client(
     "GoFile-Bot",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"]
+    bot_token=os.environ.get("BOT_TOKEN"),
+    api_id=int(os.environ.get("API_ID")),
+    api_hash=os.environ.get("API_HASH")
 )
 
 
 @Bot.on_message(filters.private & filters.command("start"))
 async def start(bot, update):
+    
     await update.reply_text(
-        text=f"Hello {update.from_user.mention}, Please send a media for gofile.io stream link.\n\nMade by @FayasNoushad",
+        text=f"""Hello {update.from_user.mention},
+        Please send a media for gofile.io stream link.\n
+        Made by @FayasNoushad""",
         disable_web_page_preview=True,
         quote=True
     )
@@ -24,15 +27,19 @@ async def start(bot, update):
 
 @Bot.on_message(filters.private & (filters.media | filters.text))
 async def filter(bot, update):
+    
     if update.text:
         if not update.text.startswith("http://") or not update.text.startswith("https://"):
             return
+    
     message = await update.reply_text(
         text="`Processing...`",
         quote=True,
         disable_web_page_preview=True
     )
+    
     try:
+        
         await message.edit_text(
             text="`Downloading...`",
             disable_web_page_preview=True
@@ -41,26 +48,30 @@ async def filter(bot, update):
             media = urldl.download(url)
         else:
             media = await update.download()
+        
         await message.edit_text(
             text="`Uploading...`",
             disable_web_page_preview=True
         )
         response = uploadFile(media)
+        
         try:
             os.remove(media)
         except:
             pass
+    
     except Exception as error:
+        
         await message.edit_text(
             text=f"Error :- `{error}`",
             quote=True,
             disable_web_page_preview=True
         )
         return
+    
     text = f"**File Name:** `{response['fileName']}`" + "\n"
     text += f"**Download Page:** `{response['downloadPage']}`" + "\n"
     text += f"**Direct Download Link:** `{response['directLink']}`" + "\n"
-    text += f"**Info:** `{response['info']}`"
     reply_markup = InlineKeyboardMarkup(
         [
             [
